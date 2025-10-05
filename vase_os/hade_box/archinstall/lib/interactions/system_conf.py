af3412de499863d1e91369594572185971a88727
h8d13 <hadrien.loge@gmail.com>
2025-10-05 14:57:37 +0200

    yes

--- vase_os/hade_box/archinstall/lib/interactions/system_conf.py
+++ vase_os/hade_box/archinstall/lib/interactions/system_conf.py
@@ -137,27 +137,27 @@ def ask_for_grub_configuration(preset: GrubConfiguration | None = None) -> GrubC
 			#)
 		#]
 
-		group = MenuItemGroup(remember_options, sort_items=False)
-		group.set_focus_by_value(preset.remember_last_selection)
+		#group = MenuItemGroup(remember_options, sort_items=False)
+		#group.set_focus_by_value(preset.remember_last_selection)
 
-		result = SelectMenu[bool](
-			group,
-			header='Remember last selected OS for next boot?\n',
-			allow_skip=True,
-			alignment=Alignment.CENTER,
-			orientation=Orientation.HORIZONTAL,
-			columns=2,
-			frame=FrameProperties.min('GRUB Memory'),
-			preview_size='auto',
-			preview_style=PreviewStyle.BOTTOM,
-			preview_frame=FrameProperties(('Info'), h_frame_style=FrameStyle.MIN),
-		).run()
+		#result = SelectMenu[bool](
+			#group,
+			#header='Remember last selected OS for next boot?\n',
+			#allow_skip=True,
+			#alignment=Alignment.CENTER,
+			#orientation=Orientation.HORIZONTAL,
+			#columns=2,
+			#frame=FrameProperties.min('GRUB Memory'),
+			#preview_size='auto',
+			#preview_style=PreviewStyle.BOTTOM,
+			#preview_frame=FrameProperties(('Info'), h_frame_style=FrameStyle.MIN),
+		#).run()
 
-		match result.type_:
-			case ResultType.Skip:
-				config.remember_last_selection = preset.remember_last_selection
-			case ResultType.Selection:
-				config.remember_last_selection = result.get_value()
+		#match result.type_:
+			#case ResultType.Skip:
+				#config.remember_last_selection = preset.remember_last_selection
+			#case ResultType.Selection:
+				#config.remember_last_selection = result.get_value()
 
 	# Ask about timeout (unless menu is hidden)
 	if not config.hide_menu:
